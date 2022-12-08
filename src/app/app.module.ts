7462c008ecdfee962b834ffbbb0159ca19a608c3
Hein Diez <heindiez@gmail.com>
2022-12-08 18:47:36 +0100

    added header and footer component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/header/header.component';
+import { LayoutComponent } from './component/layout/layout.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
